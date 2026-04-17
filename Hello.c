3dbbee21a20fc77010899daa68e06d1f22ac43ca
nira-v0601 <niravpanchal029@gmail.com>
2026-04-17 19:31:13 +0530

    Checking contribution mark

--- Hello.c
+++ Hello.c
@@ -5,6 +5,8 @@ int main()
     printf("Hello World");
     printf("\nWelcome to the world of coding");
     printf("\nThis is my first C program");
+    printf("\nI am learning C programming language");
+    printf("\nC programming is fun and rewarding");
     getch();
     return 0;
 }
