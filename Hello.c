34e0ae2747824f44331c68f2a511b4a5b125391c
nira-v0601 <niravpanchal029@gmail.com>
2026-04-18 17:02:08 +0530

    Solving Conflict

--- Hello.c
+++ Hello.c
@@ -3,10 +3,6 @@
 int main()
 {
     printf("Hello World");
-    printf("\nWelcome to the world of coding");
-    printf("\nThis is my first C program");
-    printf("\nI am learning C programming language");
-    printf("\nC programming is fun and rewarding");
     getch();
     return 0;
 }
